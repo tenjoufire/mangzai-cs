a84644ddf4d39c8a9f31861037d774a095541c76
tenjoufire <tenjoufire7@gmail.com>
2024-10-11 07:29:31 +0000

    Add initial project structure and configuration files for Azure Functions

---
+++ devdayapi-cs.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class Devdayapi_cs
+    {
+        [FunctionName("Devdayapi_cs")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string promptText = data?.text ?? string.Empty;
+
+            return new OkObjectResult("");
+        }
+    }
+}
